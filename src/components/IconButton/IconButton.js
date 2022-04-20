9f29fd7a45d8986df4f037bac83b31936e9bb59f
thanhnn1998 <thanhnn@runsystem.net>
2022-04-20 17:54:22 +0700

    fix: styling icon button

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,7 +1,6 @@
 import { forwardRef } from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 const IconButtonRoot = styled.button(
   {
     fontSize: 24,
@@ -10,29 +9,26 @@ const IconButtonRoot = styled.button(
     border: '1px solid transparent',
     outline: 'none',
     padding: '0.3rem 0.4rem',
-    color: 'var(--colorIcon)',
+    color: 'var(--defaultIcon)',
     cursor: 'pointer',
     textAlign: 'center',
     ':hover': {
       color: 'var(--primary)',
     },
-    i: {
-      width: 24,
-    },
   },
-  ({ variant, color, size, margin, radius }) => ({
+  ({ variant, color, size, radius }) => ({
     borderRadius: radius || 0,
     ...(variant === 'default' && {
       ...(color === 'success' && {
         color: 'var(--success)',
         ':hover': {
-          color: 'var(--buttonHover)',
+          color: 'var(--primaryHover)',
         },
       }),
       ...(color === 'primary' && {
         color: 'var(--primary)',
         ':hover': {
-          color: 'var(--buttonHover)',
+          color: 'var(--primaryHover)',
         },
       }),
       ...(color === 'error' && {
@@ -41,9 +37,81 @@ const IconButtonRoot = styled.button(
           color: 'var(--red)',
         },
       }),
+      ...(color === 'info' && {
+        color: 'var(--blue)',
+        ':hover': {
+          color: 'var(--blueLight)',
+        },
+      }),
+      ...(color === 'inherit' && {
+        color: 'var(--default)',
+        ':hover': {
+          color: 'var(--black)',
+        },
+      }),
+    }),
+    ...(variant === 'contained' && {
+      backgroundColor: 'var(--defaultIcon)',
+      border: '1px solid var(--defaultIcon)',
+      color: 'var(--white)',
+      ':hover': {
+        backgroundColor: 'var(--primary)',
+        color: 'var(--white)',
+        border: '1px solid var(--primary)',
+      },
+      ...(color === 'success' && {
+        backgroundColor: 'var(--success)',
+        border: '1px solid var(--success)',
+        color: 'var(--white)',
+        ':hover': {
+          backgroundColor: 'var(--primaryHover)',
+          border: '1px solid var(--primaryHover)',
+          color: 'var(--white)',
+        },
+      }),
+      ...(color === 'primary' && {
+        backgroundColor: 'var(--primary)',
+        border: '1px solid var(--primary)',
+        color: 'var(--white)',
+        ':hover': {
+          backgroundColor: 'var(--primaryHover)',
+          color: 'var(--white)',
+          border: '1px solid var(--primaryHover)',
+        },
+      }),
+      ...(color === 'error' && {
+        backgroundColor: 'var(--danger)',
+        border: '1px solid var(--danger)',
+        color: 'var(--white)',
+        ':hover': {
+          backgroundColor: 'var(--red)',
+          color: 'var(--white)',
+          border: '1px solid var(--red)',
+        },
+      }),
+      ...(color === 'info' && {
+        backgroundColor: 'var(--blue)',
+        border: '1px solid var(--blue)',
+        color: 'var(--white)',
+        ':hover': {
+          backgroundColor: 'var(--blueLight)',
+          color: 'var(--white)',
+          border: '1px solid var(--blueLight)',
+        },
+      }),
+      ...(color === 'inherit' && {
+        backgroundColor: 'var(--default)',
+        border: '1px solid var(--default)',
+        color: 'var(--white)',
+        ':hover': {
+          backgroundColor: 'var(--black)',
+          color: 'var(--white)',
+          border: '1px solid var(--black)',
+        },
+      }),
     }),
     ...(variant === 'outlined' && {
-      border: '1px solid var(--colorIcon)',
+      border: '1px solid var(--defaultIcon)',
       ':hover': {
         border: '1px solid var(--primary)',
       },
@@ -51,16 +119,16 @@ const IconButtonRoot = styled.button(
         color: 'var(--success)',
         border: '1px solid var(--success)',
         ':hover': {
-          color: 'var(--buttonHover)',
-          border: '1px solid var(--buttonHover)',
+          color: 'var(--primaryHover)',
+          border: '1px solid var(--primaryHover)',
         },
       }),
       ...(color === 'primary' && {
         color: 'var(--primary)',
         border: '1px solid var(--primary)',
         ':hover': {
-          color: 'var(--buttonHover)',
-          border: '1px solid var(--buttonHover)',
+          color: 'var(--primaryHover)',
+          border: '1px solid var(--primaryHover)',
         },
       }),
       ...(color === 'error' && {
@@ -69,173 +137,64 @@ const IconButtonRoot = styled.button(
         ':hover': {
           color: 'var(--red)',
           border: '1px solid var(--red)',
+        },
+      }),
+      ...(color === 'info' && {
+        color: 'var(--blue)',
+        border: '1px solid var(--blue)',
+        ':hover': {
+          color: 'var(--blueLight)',
+          border: '1px solid var(--blueLight)',
+        },
+      }),
+      ...(color === 'inherit' && {
+        color: 'var(--default)',
+        border: '1px solid var(--default)',
+        ':hover': {
+          color: 'var(--black)',
+          border: '1px solid var(--black)',
         },
       }),
     }),
     ...(size === 'small' && {
       fontSize: 20,
       padding: '0.1rem 0.2rem',
-      i: {
-        width: 20,
-      },
     }),
     ...(size === 'medium' && {
       fontSize: 24,
       padding: '0.3rem 0.4rem',
-      i: {
-        width: 24,
-      },
     }),
     ...(size === 'large' && {
       fontSize: 28,
       padding: '20px',
-      i: {
-        width: 28,
-      },
-    }),
-    ...(margin === 'ml-auto' && {
-      marginLeft: 'auto',
-    }),
-    ...(margin === 'mr-auto' && {
-      marginRight: 'auto',
-    }),
-  })
-);
-const LinkRoot = styled(({ children, ...props }) => (
-  <NavLink {...props}>{children}</NavLink>
-))(
-  {
-    fontSize: 24,
-    lineHeight: 1,
-    backgroundColor: 'transparent',
-    border: '1px solid transparent',
-    outline: 'none',
-    padding: '0.3rem 0.4rem',
-    color: 'var(--colorIcon)',
-    cursor: 'pointer',
-    textDecoration: 'none',
-    textAlign: 'center',
-    ':hover': {
-      color: 'var(--primary)',
-    },
-    i: {
-      width: 24,
-    },
-  },
-  ({ variant, color, size, margin, radius }) => ({
-    borderRadius: radius || 0,
-    ...(variant === 'default' && {
-      ...(color === 'success' && {
-        color: 'var(--success)',
-        ':hover': {
-          color: 'var(--buttonHover)',
-        },
-      }),
-      ...(color === 'primary' && {
-        color: 'var(--primary)',
-        ':hover': {
-          color: 'var(--buttonHover)',
-        },
-      }),
-      ...(color === 'error' && {
-        color: 'var(--danger)',
-        ':hover': {
-          color: 'var(--red)',
-        },
-      }),
-    }),
-    ...(variant === 'outlined' && {
-      border: '1px solid var(--colorIcon)',
-      ':hover': {
-        border: '1px solid var(--primary)',
-      },
-      ...(color === 'success' && {
-        color: 'var(--success)',
-        border: '1px solid var(--success)',
-        ':hover': {
-          color: 'var(--buttonHover)',
-          border: '1px solid var(--buttonHover)',
-        },
-      }),
-      ...(color === 'primary' && {
-        color: 'var(--primary)',
-        border: '1px solid var(--primary)',
-        ':hover': {
-          color: 'var(--buttonHover)',
-          border: '1px solid var(--buttonHover)',
-        },
-      }),
-      ...(color === 'error' && {
-        color: 'var(--danger)',
-        border: '1px solid var(--danger)',
-        ':hover': {
-          color: 'var(--red)',
-          border: '1px solid var(--red)',
-        },
-      }),
-    }),
-    ...(size === 'small' && {
-      fontSize: 20,
-      padding: 'calc(0.1rem - 2px) 0.2rem',
-      i: {
-        width: 20,
-      },
-    }),
-    ...(size === 'medium' && {
-      fontSize: 24,
-      padding: 'calc(0.3rem - 2px) 0.4rem',
-      i: {
-        width: 24,
-      },
-    }),
-    ...(size === 'large' && {
-      fontSize: 28,
-      padding: '18px 20px',
-      i: {
-        width: 28,
-      },
-    }),
-    ...(margin === 'ml-auto' && {
-      marginLeft: 'auto',
-    }),
-    ...(margin === 'mr-auto' && {
-      marginRight: 'auto',
     }),
   })
 );
 const IconButton = forwardRef(
-  ({ variant, color, size, margin, children, radius, ...props }, ref) =>
-    props.to ? (
-      <LinkRoot
-        variant={variant}
-        color={color}
-        size={size}
-        margin={margin}
-        radius={radius}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </LinkRoot>
-    ) : (
-      <IconButtonRoot
-        variant={variant}
-        color={color}
-        size={size}
-        margin={margin}
-        radius={radius}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </IconButtonRoot>
-    )
+  ({ variant, color, size, children, radius, ...props }, ref) => (
+    <IconButtonRoot
+      variant={variant}
+      color={color}
+      size={size}
+      radius={radius}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </IconButtonRoot>
+  )
 );
 IconButton.propTypes = {
   variant: PropTypes.oneOf(['default', 'contained', 'outlined']),
-  color: PropTypes.oneOf(['default', 'primary', 'success', 'error']),
+  color: PropTypes.oneOf([
+    'default',
+    'primary',
+    'success',
+    'error',
+    'info',
+    'inherit',
+  ]),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
-  margin: PropTypes.oneOf(['ml-auto', 'mr-auto']),
   radius: PropTypes.number,
 };
 IconButton.defaultProps = {
