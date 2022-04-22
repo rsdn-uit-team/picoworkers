c28200570ec32a3204e3e4654dc7195c365c87d2
thanhnn1998 <thanhnn@runsystem.net>
2022-04-22 11:59:54 +0700

    feat: add component icons and reddit icon

---
+++ src/components/Icons/RedditIcon/RedditIcon.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import RedditIcon from './RedditIcon';
+
+export default {
+  title: 'Components/Icons/RedditIcon',
+  component: RedditIcon,
+};
+
+export const Default = (props) => <RedditIcon {...props} />;
