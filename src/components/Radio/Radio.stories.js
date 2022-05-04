972038342f89a53a01f03b565dec7efb3ed0590b
thanhnn1998 <thanhnn@runsystem.net>
2022-05-04 11:32:43 +0700

    feat: add component radio button

---
+++ src/components/Radio/Radio.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Radio from './Radio';
+
+export default {
+  title: 'Components/Radio',
+  component: Radio,
+};
+
+export const Default = (props) => <Radio id="radio" {...props} />;
